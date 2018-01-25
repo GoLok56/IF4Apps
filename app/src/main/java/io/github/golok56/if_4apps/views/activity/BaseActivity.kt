2aad4905eb4c60a4506753f2d8f7a56433393104
Satria Adi Putra <satadii11@gmail.com>
2018-01-25 19:32:40 +0700

    add base activity

---
+++ app/src/main/java/io/github/golok56/if_4apps/views/activity/BaseActivity.kt
@@ -0,0 +1,13 @@
+package io.github.golok56.if_4apps.views.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+
+/**
+ * @author Satria Adi Putra
+ */
+class BaseActivity : AppCompatActivity() {
+    fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
