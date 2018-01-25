cf96dda823bdd3719a94e77f8b0559dadb46c717
Satria Adi Putra <satadii11@gmail.com>
2018-01-25 19:40:04 +0700

    add login activity

---
+++ app/src/main/java/io/github/golok56/if_4apps/views/activity/LoginActivity.kt
@@ -0,0 +1,13 @@
+package io.github.golok56.if_4apps.views.activity
+
+import android.os.Bundle
+
+/**
+ * @author Satria Adi Putra
+ */
+class LoginActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
