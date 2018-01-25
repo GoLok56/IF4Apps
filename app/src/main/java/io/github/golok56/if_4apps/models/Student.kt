c1cf5d7b7bb97055cbec02dc52237c0ae16c40ca
Satria Adi Putra <satadii11@gmail.com>
2018-01-25 22:44:33 +0700

    add student model

---
+++ app/src/main/java/io/github/golok56/if_4apps/models/Student.kt
@@ -0,0 +1,10 @@
+package io.github.golok56.if_4apps.models
+
+/**
+ * @author Satria Adi Putra
+ */
+data class Student(
+        var nim: String,
+        var name: String,
+        var password: String
+)
