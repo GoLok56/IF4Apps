4d3677a7fcb270305c37c2eeb3d7b2e6eee9533e
Satria Adi Putra <satadii11@gmail.com>
2018-01-25 20:38:18 +0700

    add login layout

--- app/src/main/java/io/github/golok56/if_4apps/views/activity/LoginActivity.kt
+++ app/src/main/java/io/github/golok56/if_4apps/views/activity/LoginActivity.kt
@@ -1,6 +1,8 @@
 package io.github.golok56.if_4apps.views.activity
 
 import android.os.Bundle
+import io.github.golok56.if_4apps.R
+import kotlinx.android.synthetic.main.login_activity.*
 
 /**
  * @author Satria Adi Putra
@@ -8,6 +10,8 @@ import android.os.Bundle
 class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
 
+        btnLogin.setOnClickListener{ showToast("Aduh sakit") }
     }
 }
