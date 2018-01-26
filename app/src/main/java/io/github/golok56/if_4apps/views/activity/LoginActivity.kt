76f44e62a49b7462d0a89ea690332056cdb7fe64
Satria Adi Putra <satadii11@gmail.com>
2018-01-26 11:40:19 +0700

    modify loginactivity to contain fragment

--- app/src/main/java/io/github/golok56/if_4apps/views/activity/LoginActivity.kt
+++ app/src/main/java/io/github/golok56/if_4apps/views/activity/LoginActivity.kt
@@ -1,8 +1,9 @@
 package io.github.golok56.if_4apps.views.activity
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import io.github.golok56.if_4apps.R
-import kotlinx.android.synthetic.main.login_activity.*
+import io.github.golok56.if_4apps.views.fragment.NimFragment
 
 /**
  * @author Satria Adi Putra
@@ -10,8 +11,16 @@ import kotlinx.android.synthetic.main.login_activity.*
 class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_activity)
+        setContentView(R.layout.activity_login)
 
-        btnLogin.setOnClickListener{ showToast("Aduh sakit") }
+        supportFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainer, NimFragment(), NimFragment.TAG)
+                .commit()
+    }
+
+    fun show(fragment : Fragment, tag : String) {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, fragment, tag)
+                .commit()
     }
 }
